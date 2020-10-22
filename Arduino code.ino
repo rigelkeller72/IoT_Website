6409f8c65651078c144b809557a08f7bbc058ae3
minimally228 <pmmalatesta228@gmail.com>
2020-10-22 14:17:00 -0400

    Adding arduino code and current reading from serial framework.

---
+++ Arduino code.ino
@@ -0,0 +1,96 @@
+
+#include <dht.h>
+
+dht DHT;
+float temp,hum;
+#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 3 //attach pin D3 Arduino to pin Trig of HC-SR04
+long duration;
+int distance,inp;
+float pot;
+int potpin = A5;
+char data[20];
+#define DHT11_PIN 7
+void setup() {
+   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
+  pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
+  pinMode(LED_BUILTIN, OUTPUT);
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+}
+
+void loop() {
+  inp = Serial.read();
+  switch (inp)
+  {
+    case 'r':
+      readsensors();
+      break;
+    case 'l':
+      digitalWrite(LED_BUILTIN, HIGH);
+      break;
+    case 'o':
+      digitalWrite(LED_BUILTIN, LOW);
+      break;
+  }
+
+  /*
+  // put your main code here, to run repeatedly:
+    // Clears the trigPin condition
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  dht DHT;
+  #define DHT11_PIN 7
+  // Displays the distance on the Serial Monitor
+  int test1 = DHT.read11(DHT11_PIN);
+  temp = DHT.temperature;
+  hum = DHT.humidity;
+  pot = analogRead(potpin)/1023.0;
+  Serial.print(distance);
+  Serial.print(",");
+  Serial.print(temp);
+  Serial.print(",");
+  Serial.print(hum);
+  Serial.print(",");
+  Serial.println(pot);
+  Serial.flush();
+  */
+  
+}
+void readsensors(){
+      // Clears the trigPin condition
+      digitalWrite(trigPin, LOW);
+      delayMicroseconds(2);
+      // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+      digitalWrite(trigPin, HIGH);
+      delayMicroseconds(10);
+      digitalWrite(trigPin, LOW);
+      // Reads the echoPin, returns the sound wave travel time in microseconds
+      duration = pulseIn(echoPin, HIGH);
+      // Calculating the distance
+      distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+      dht DHT;
+      #define DHT11_PIN 7
+      // Displays the distance on the Serial Monitor
+      int test1 = DHT.read11(DHT11_PIN);
+      temp = DHT.temperature;
+      hum = DHT.humidity;
+      pot = analogRead(potpin)/1023.0;
+      Serial.print(distance);
+      Serial.print(",");
+      Serial.print(temp);
+      Serial.print(",");
+      Serial.print(hum);
+      Serial.print(",");
+      Serial.println(pot);
+      Serial.flush();
+  }
---
+++ donwork.py
@@ -0,0 +1,28 @@
+import serial
+DEVICE = 'COM8'
+ser = serial.Serial(DEVICE)
+while True:
+    #Reads in serial data
+    rxdata = ser.readline()
+    str = rxdata.decode('ascii')
+    #converts to string and splits comma deliniated
+    chunks = str.split(',')
+    sensVals =[]
+    #casts strings into floats
+    for sense in chunks:
+        sensVals.append(float(sense))
+    sensVals[3] = sensVals[3]*100
+    #prints the values sent
+    print("The distance is %f cm.\nThe temperature is %f degrees C" %(sensVals[0],sensVals[1]))
+    print("The humidity is %f percent\nThe potentiometer is %f percent" %(sensVals[2],sensVals[3]))
+    if sensVals[0] < 10:
+        #sends info to turn on light/turn off light
+        print("Back up")
+        serprint = ('l'+'\n').encode('ascii')
+        ser.flush()
+        ser.write(serprint)
+        ser.flush()
+    else:
+        serprint = ('g' + '\n').encode('ascii')
+        ser.write(serprint)
+        ser.flush()
