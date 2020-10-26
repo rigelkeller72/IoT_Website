56660944ce39e33a1f840e2cc05d9910cf0753af
minimally228 <pmmalatesta228@gmail.com>
2020-10-26 16:50:39 -0400

    Still broken-arduiono won't enter switch unless there's a print before it, stratching my head about that one.

--- donwork.py
+++ donwork.py
@@ -3,28 +3,19 @@ DEVICE = 'COM8'
 ser = serial.Serial(DEVICE)
 while True:
     #Reads in serial data
-    serprint = ('r' + '\n').encode('ascii')
+    serprint = ('r').encode('ascii')
+    ser.write(serprint)
     rxdata = ser.readline()
     str = rxdata.decode('ascii')
+    print(str)
     #converts to string and splits comma deliniated
-    chunks = str.split(',')
-    sensVals =[]
+    #chunks = str.split(',')
+    #sensVals =[]
     #casts strings into floats
-    for sense in chunks:
-        sensVals.append(float(sense))
-    sensVals[3] = sensVals[3]*100
-    #prints the values sent
-    print("The distance is %f cm.\nThe temperature is %f degrees C" %(sensVals[0],sensVals[1]))
-    print("The humidity is %f percent\nThe potentiometer is %f percent" %(sensVals[2],sensVals[3]))
-    if sensVals[0] < 10:
-        #sends info to turn on light/turn off light
-        print("Back up")
-        serprint = ('l'+'\n').encode('ascii')
-        ser.flush()
-        ser.write(serprint)
-        ser.flush()
-    else:
+    #for sense in chunks:
+    #    sensVals.append(float(sense))
+    #sensVals[3] = sensVals[3]*100
+    ##prints the values sent
+    #print("The distance is %f cm.\nThe temperature is %f degrees C" %(sensVals[0],sensVals[1]))
+    #print("The humidity is %f percent\nThe potentiometer is %f percent" %(sensVals[2],sensVals[3]))
 
-
-        ser.write(serprint)
-        ser.flush()
--- hello.py
+++ hello.py
@@ -1,23 +1,23 @@
 from flask import Flask
 import serial
 from flask import request
-app = Flask(__name__)
+app = Flask(__name__)   
 
 
 @app.route('/', methods=['GET'])
 def hello_world():
     DEVICE = 'COM8'
     ser = serial.Serial(DEVICE)
-    serprint = ('r' + '\n').encode('ascii')
+    serprint = ('r').encode('ascii')
     ser.write(serprint)
     rxdata = ser.readline()
     str = rxdata.decode('ascii')
     # converts to string and splits comma deliniated
-    chunks = str.split(',')
-    sensVals = []
+    #chunks = str.split(',')
+    #sensVals = []
     # casts strings into floats
-    for sense in chunks:
-        sensVals.append(float(sense))
-    sensVals[3] = sensVals[3] * 100
-    ser.close()
+    #for sense in chunks:
+    #    sensVals.append(float(sense))
+    #sensVals[3] = sensVals[3] * 100
+    #ser.close()
     return str
